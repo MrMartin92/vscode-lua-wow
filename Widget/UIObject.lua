cec3d81a9ecfc55c0ab9b7a6606ef9bb969bb32f
MrMartinZockt <Tobi_Lorenz@t-online.de>
2020-01-25 12:06:14 +0100

    Added UIObject

---
+++ Widget/UIObject.lua
@@ -0,0 +1,33 @@
+--- UIObject is the base of all user interface elements.
+---@class UIObject
+UIObject = {}
+
+--- Returns the object's _G accessible path.
+---@return string debugName
+function UIObject:GetDebugName() end
+
+--- Returns the widget object's name
+---@result string name
+function UIObject:GetName() end
+
+--- Returns the object's widget type
+---@return string type
+function UIObject:GetObjectType() end
+
+--- Returns the object's parent object.
+---@return UIObject|nil parent
+function UIObject:GetParent() end
+
+--- Returns whether insecure interaction with a widget is forbidden.
+---@return boolean isForbidden @true if this widget's methods may only be called from secure execution paths, false otherwise.
+---[View Documentation](https://wow.gamepedia.com/API_UIObject_IsForbidden)
+function UIObject:IsForbidden() end
+
+--- Returns whether the object belongs to a given widget type, or a subclass of that type.
+---@param type string
+---@return boolean isType
+function UIObject:IsObjectType(type) end
+
+--- PROTECTED
+---@param status boolean
+function UIObject:SetForbidden(status) end
