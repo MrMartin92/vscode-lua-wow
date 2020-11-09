9eed0b20303ac66088cccfecb64687fe9df3fbcc
Tobias Lorenz <4492249+MrMartin92@users.noreply.github.com>
2020-11-09 23:28:26 +0100

    Added CalendarEventIndexInfo struct

---
+++ structs/CalendarEventIndexInfo.lua
@@ -0,0 +1,6 @@
+---[Wowpedia documentation](https://wow.gamepedia.com/Struct_CalendarEventIndexInfo)
+---@class CalendarEventIndexInfo
+---@field public offsetMonth number
+---@field public monthDay number
+---@field public eventIndex number
+CalendarEventIndexInfo = {}
