b4f35a7d296f3387f97133e68f124725e371ae87
Tobias Lorenz <4492249+MrMartin92@users.noreply.github.com>
2020-11-09 23:20:06 +0100

    Added CalenderTime struct

---
+++ structs/CalendarTime.lua
@@ -0,0 +1,9 @@
+---[Wowpedia documentation](https://wow.gamepedia.com/Struct_CalendarTime)
+---@class CalendarTime
+---@field public year number @The current year (2019)
+---@field public month number @The current month (1-12)
+---@field public monthDay number @The Curennt day of the month (1-31)
+---@field public weekday number @The current day of the week (1=Sunday, 2=Monday, ..., 7=Saturday)
+---@field public hour number @The current time in hours (0-23)
+---@field public minute number @The current time in minutes (0-59)
+CalendarTime = {}
