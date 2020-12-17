b279bc1528f28f45befb2760b08475d698a39fd7
Tobias Lorenz <4492249+MrMartin92@users.noreply.github.com>
2020-12-17 19:13:39 +0100

    Remove "CalendarEventIndexInfo.lua" File

--- structs/CalendarEventIndexInfo.lua
+++
@@ -1,6 +0,0 @@
----[Wowpedia documentation](https://wow.gamepedia.com/Struct_CalendarEventIndexInfo)
----@class CalendarEventIndexInfo
----@field public offsetMonth number
----@field public monthDay number
----@field public eventIndex number
-CalendarEventIndexInfo = {}
